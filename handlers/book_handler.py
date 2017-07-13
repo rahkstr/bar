920d5d8211dc2c6ca9236402e02ff5c31baefe61
rahkstr <rahkstr@gmail.com>
2017-07-13 14:35:01 -0400

    Add html to main template

--- handlers/book_handler.py
+++ handlers/book_handler.py
@@ -7,9 +7,9 @@ from models import book
 
 class BookHandler(webapp2.RequestHandler):
     def get(self):
-    	logging.info("SecondHandler")
-    	books = book.Book.query().fetch()
-    	# do stuff with books...
+        logging.info("SecondHandler")
+        books = book.Book.query().fetch()
+        # do stuff with books...
         html_params = {
             "title": "Second Title",
             "content": "Some stuff about books."
